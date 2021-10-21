cda13d170ecb73fcba2bd008cec8668107af0e76
Martin <martinlearmont@hotmail.com>
2021-10-21 15:23:30 +0100

    Added implementation of promotion engine

---
+++ PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngine
+{
+    public class PromotionEngine : IPromotionEngine
+    {
+        private readonly List<IPromotionRule> _promotionRules;
+        public PromotionEngine(List<IPromotionRule> promotionRules)
+        {
+            _promotionRules = promotionRules ?? new List<IPromotionRule>();
+        }
+
+        public decimal CalculateDiscount(List<string> skus)
+        {
+            var applicablePromotions = _promotionRules.Where(rule => rule.IsMatch(skus));
+            return applicablePromotions.Sum(rule => rule.CalculateDiscount(skus));
+        }
+    }
+}
