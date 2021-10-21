3b387bcd8baf60c5bbf5cde4ced5aba81e3adb2f
Martin <martinlearmont@hotmail.com>
2021-10-21 14:32:31 +0100

    Implemented CalculateDiscount functionality for PromotionRule.

--- PromotionEngine/PromotionRule.cs
+++ PromotionEngine/PromotionRule.cs
@@ -5,15 +5,26 @@ namespace PromotionEngine
 {
     public class PromotionRule : IPromotionRule
     {
-        private readonly Predicate<List<string>> _promotionCondition;       
+        private readonly Predicate<List<string>> _promotionCondition;
+        private readonly Func<List<string>, Dictionary<string, decimal>, decimal> _promotionDiscount;
+        private readonly Dictionary<string, decimal> _unitPrices;
 
-        public PromotionRule(Predicate<List<string>> promotionCondition)
+        public PromotionRule(Predicate<List<string>> promotionCondition, Func<List<string>,
+            Dictionary<string, decimal>, decimal> promotionDiscount,
+            Dictionary<string, decimal> unitPrices)
         {
             _promotionCondition = promotionCondition;
+            _promotionDiscount = promotionDiscount;
+            _unitPrices = unitPrices;
         }
         public bool IsMatch(List<string> skus)
         {
             return _promotionCondition(skus);
-        }     
+        }
+
+        public decimal CalculateDiscount(List<string> skus)
+        {
+            return _promotionDiscount(skus, _unitPrices);
+        }
     }
 }
