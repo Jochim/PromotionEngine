832d6030eac261954998d1ff9c1a274a96e6c143
Martin <martinlearmont@hotmail.com>
2021-10-21 13:46:58 +0100

    Added AddItems/GetItems functionality to Cart. Updated test case to pass.

--- PromotionEngine/Cart.cs
+++ PromotionEngine/Cart.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PromotionEngine
 {
+    /// <summary>
+    /// Group and manage lists of skus.
+    /// </summary>
     public class Cart
     {
+        private List<string> _skus = new();
+
+        public void AddItems(List<string> skus)
+        {
+            _skus.AddRange(skus);
+        }
+
+        public List<string> GetItems()
+        {
+            return new(_skus);
+        }
     }
 }
--- PromotionEngineTests/CartTests.cs
+++ PromotionEngineTests/CartTests.cs
@@ -1,3 +1,4 @@
+using PromotionEngine;
 using System.Collections.Generic;
 using Xunit;
 
