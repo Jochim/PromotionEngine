b876a0c25ebd7c0f59d8d11164ab6ed4a89f45c1
Martin <martinlearmont@hotmail.com>
2021-10-21 14:16:50 +0100

    Added failing tests for PromotionRule matching.

---
+++ PromotionEngineTests/PromotionRuleTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace PromotionEngineTests
+{
+    public class PromotionRuleTests
+    {
+        [Fact]
+        public void RuleIsMatchIsTrue()
+        {
+            var promotionRule = new PromotionRule(()=> true);
+            Assert.True(promotionRule.IsMatch(new()));
+        }
+
+        [Fact]
+        public void RuleIsMatchIsFalse()
+        {
+            var promotionRule = new PromotionRule(() => false);
+            Assert.False(promotionRule.IsMatch(new()));
+        }
+
+        
+
+
+    }
+}
